f2624c42dfe2e489dbc49eabb40d50c413e27219
Milo Rastgoo <hello@informagician.com>
2019-12-02 15:51:13 -0500

    midde image added

--- js/index.js
+++ js/index.js
@@ -56,3 +56,5 @@ document.querySelector(".text-content:nth-child(1) > h4").textContent = siteCont
 document.querySelector(".text-content:nth-child(1) > p").textContent = siteContent["main-content"]["features-content"];
 document.querySelector(".text-content:nth-child(2) > h4").textContent = siteContent["main-content"]["about-h4"];
 document.querySelector(".text-content:nth-child(2) > p").textContent = siteContent["main-content"]["about-content"];
+
+document.getElementById("middle-img").setAttribute('src', siteContent["main-content"]["middle-img-src"]);
